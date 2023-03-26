9c187325210fe549aa7efc1a97c52d3d795b8205
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-03-26 13:53:52 +0400

    Added exception handling.

--- script.py
+++ script.py
@@ -7,13 +7,51 @@ django.setup()
 from datacenter.models import (Chastisement, Commendation, Lesson, Mark, Schoolkid, Subject)
 
 
-def get_child(child):
-    kid = Schoolkid.objects.get(full_name__contains=child)
-    return kid
+commendations = [' Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
+                 'Великолепно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
+                 'Сказано здорово – просто и ясно!', 'Прекрасно!', 'Ты, как всегда, точен!',
+                 'Очень хороший ответ!', 'Талантливо!Молодец!', 'Ты сегодня прыгнул выше головы!',
+                 'Я поражен!', 'Замечательно!']
+
+
+def get_child(schoolkid_name):
+    schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    try:
+        fix_marks(schoolkid)
+        print('Ученик найден')
+        print('Оценки исправлены')
+        remove_chastisements(schoolkid)
+        print('Замечания удалены')
+    except Schoolkid.DoesNotExist:
+        print('Ученик не найден. '
+              f'Пожалуйста проверьте имя ученика. Имя {schoolkid} некорректно. '
+              'Для поиска необходимо использовать как имя, '
+              'так и фамилию ученика.'.format(schoolkid=schoolkid),
+              'так и фамилию ученика.'.format(schoolkid=schoolkid_surname_name))
+    except Schoolkid.MultipleObjectsReturned:
+        print('Найдено несколько учеников по данному запросу. '
+              f'Пожалуйста проверьте имя ученика. Имя {schoolkid} некорректно.'
+              'Для поиска необходимо использовать как имя, так и фамилию'
+              'ученика.'.format(schoolkid=schoolkid))
+    return schoolkid
 
 
 def find_subject(subject, child):
+    input_schoolkid_subject = parse_user_input().subject
     subjects = Subject.objects.get(title=subject, year_of_study=child.year_of_study)
+    try:
+        if input_schoolkid_subject:
+            create_commendation(schoolkid_name, input_schoolkid_subject)
+        print('Благодарность присвоена')
+    except Subject.DoesNotExist:
+        print('Предмет не найден. Пожалуйста проверьте название предмета.'
+              f'Название предмета {subject} некорректно. Для поиска необходимо'
+              'использовать корректное название.'.format(subject=subject))
+    except Subject.MultipleObjectsReturned:
+        print('Найдено несколько предметов по данному запросу.'
+              'Пожалуйста проверьте название предмета. Название'
+              f'предмета {subject} некорректно. Для поиска необходимо '
+              'использовать корректное название.'.format(subject=subject))
     return subjects
 
 
@@ -55,65 +93,10 @@ def parse_user_input():
 
 
 if __name__ == '__main__':
-    commendations = [' Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
-                     'Великолепно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
-                     'Сказано здорово – просто и ясно!', 'Прекрасно!', 'Ты, как всегда, точен!',
-                     'Очень хороший ответ!', 'Талантливо!Молодец!', 'Ты сегодня прыгнул выше головы!',
-                     'Я поражен!', 'Замечательно!']
-    input_schoolkid_subject = parse_user_input()
-    schoolkid_surname_name = input_schoolkid_subject.schoolkid_surname_name
     try:
         input_schoolkid_subject = parse_user_input()
-        schoolkid = input_schoolkid_subject['schoolkid']
+        schoolkid_name = input_schoolkid_subject['schoolkid']
+        schoolkid_surname_name = input_schoolkid_subject.schoolkid_surname_name
     except SystemExit:
         print('Программа завершила работу неправильно, проверьте задаваемые атрибуты')
         exit()
-    try:
-        fix_marks(get_child(schoolkid))
-        schoolkid = get_child(schoolkid_surname_name)
-        fix_marks(schoolkid)
-        print('Ученик найден')
-        print('Оценки исправлены')
-        remove_chastisements(get_child(schoolkid))
-        remove_chastisements(schoolkid)
-        print('Замечания удалены')
-        try:
-            if input_schoolkid_subject.subject:
-                subject = input_schoolkid_subject.subject
-                create_commendation(schoolkid, find_subject(subject, schoolkid))
-            print('Благодарность присвоена')
-        except Subject.DoesNotExist:
-            print('Предмет не найден. Пожалуйста проверьте название предмета.'
-                  f'Название предмета {subject} некорректно. Для поиска необходимо'
-                  'использовать корректное название.'.format(subject=subject))
-        except Subject.MultipleObjectsReturned:
-            print('Найдено несколько предметов по данному запросу.'
-                  'Пожалуйста проверьте название предмета. Название'
-                  f'предмета {subject} некорректно. Для поиска необходимо '
-                  'использовать корректное название.'.format(subject=subject))
-    except Schoolkid.DoesNotExist:
-        print('Ученик не найден. '
-              f'Пожалуйста проверьте имя ученика. Имя {schoolkid} некорректно. '
-              'Для поиска необходимо использовать как имя, '
-              'так и фамилию ученика.'.format(schoolkid=schoolkid),
-              'так и фамилию ученика.'.format(schoolkid=schoolkid_surname_name))
-    except Schoolkid.MultipleObjectsReturned:
-        print('Найдено несколько учеников по данному запросу. '
-              f'Пожалуйста проверьте имя ученика. Имя {schoolkid} некорректно.'
-              'Для поиска необходимо использовать как имя, так и фамилию'
-              'ученика.'.format(schoolkid=schoolkid))
-    try:
-        if input_schoolkid_subject.get('subject'):
-            subject = input_schoolkid_subject['subject']
-            create_commendation(get_child(schoolkid), find_subject(subject, get_child(schoolkid)))
-            print('Благодарность присвоена')
-    except Subject.DoesNotExist:
-        print('Предмет не найден. Пожалуйста проверьте название предмета.'
-              f'Название предмета {subject} некорректно. Для поиска необходимо'
-              'использовать корректное название.'.format(subject=subject))
-    except Subject.MultipleObjectsReturned:
-        print('Найдено несколько предметов по данному запросу.'
-              'Пожалуйста проверьте название предмета. Название'
-              f'предмета {subject} некорректно. Для поиска необходимо '
-              'использовать корректное название.'.format(subject=subject),
-              'ученика.'.format(schoolkid=schoolkid_surname_name))
